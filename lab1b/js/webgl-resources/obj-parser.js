abfa3bed99ab4171d7e5152a7bec79f4ca45a1b8
Florian Hejze <florian.hejze@gmx.at>
2024-11-15 22:39:11 +0100

    probably fixed

--- lab1b/js/webgl-resources/obj-parser.js
+++ lab1b/js/webgl-resources/obj-parser.js
@@ -1,106 +1,179 @@
+import { distance } from "../gl-matrix/dist/esm/vec3.js";
 import { Shape } from "./shape.js";
 
 export class OBJParser {
     min = [0.0,0.0,0.0];
     max = [0.0,0.0,0.0];
+    maxDistance = 0.0;
+    vertexMap = new Map();
+
+    rawVertices = [];
+    rawNormals = [];
+    rawVertexIndices = [];
+    rawNormalIndices = [];
+    
     vertices = [];
-    colors = [];
-    indices = [];
     normals = [];
+    indices = [];
+    colors = [];
 
     reset() {
         this.min = [0.0,0.0,0.0];
         this.max = [0.0,0.0,0.0];
+        this.maxDistance = 0.0;
+        this.vertexMap = new Map();
+
+        this.rawVertices = [];
+        this.rawNormals = [];
+        this.rawVertexIndices = [];
+        this.rawNormalIndices = [];
+        
         this.vertices = [];
         this.normals = [];
-        this.colors = [];
         this.indices = [];
-    }   
+        this.colors = [];
+    }
 
     async parseObjectFromFile(filePath) {
        return this.parseObjectFromString(await (fetch(filePath).then(file => file.text())));
     }
 
+    /**
+     * 
+     * @param {String[]} line 
+     */
+    parseVertex(line) {
+        const [x, y, z] = line.slice(1,4).map(Number);
+
+        if (this.max[0] < x) {
+            this.max[0] = x;
+        } else if (x < this.min[0]) {
+            this.min[0] = x;
+        }
+
+        if (this.max[1] < y) {
+            this.max[1] = y;
+        } else if (y < this.min[1]) {
+            this.min[1] = y;
+        }
+
+        if (this.max[2] < z) {
+            this.max[2] = z;
+        } else if (z < this.min[2]) {
+            this.min[2] = z;
+        }
+
+        const distance = Math.sqrt(x*x +y*y+z*z);
+
+        if (distance > this.maxDistance) {
+            this.maxDistance = distance;
+        }
+
+        this.rawVertices.push(x);
+        this.rawVertices.push(y);
+        this.rawVertices.push(z);
+    }
+
+    /**
+     * 
+     * @param {String[]} line 
+     */
+    parseNormal(line) {
+        const [x, y, z] = line.slice(1,4).map(Number);
+
+        this.rawNormals.push(x);
+        this.rawNormals.push(y);
+        this.rawNormals.push(z);
+    }
+
+    /**
+     * 
+     * @param {String[]} line 
+     */
+    parseFace(line) {
+        for (let i = 1; i < line.length; i++) {
+            const [vertexIndex,,normalIndex] = line[i].split('/').map(Number);
+
+            this.rawVertexIndices.push(vertexIndex-1);
+            this.rawNormalIndices.push(normalIndex-1);
+        }
+    }
+
+    normalize() {
+        const xOffset = (this.max[0] + this.min[0])/2;
+        const yOffset = (this.max[1] + this.min[1])/2;
+        const zOffset = (this.max[2] + this.min[2])/2;
+
+        for (let i = 0; i < this.rawVertices.length; i += 3) {
+            this.rawVertices[i] -= xOffset;
+            this.rawVertices[i + 1] -= yOffset;
+            this.rawVertices[i + 2] -= zOffset;
+
+            this.rawVertices[i] /= this.maxDistance;
+            this.rawVertices[i + 1] /= this.maxDistance;
+            this.rawVertices[i + 2] /= this.maxDistance;
+        }
+    }
+
+    combine() {
+        for(let i = 0; i < this.rawVertexIndices.length; i++) {
+            this.vertices.push(...this.rawVertices.slice(this.rawVertexIndices[i]*3, this.rawVertexIndices[i]*3 +3));
+            this.normals.push(...this.rawNormals.slice(this.rawNormalIndices[i]*3,this.rawNormalIndices[i]*3+3));
+            this.indices.push(i);
+            this.colors.push(...[0.0,1.0,1.0,1.0]);
+            //this.colors.push(...[0.0,1.0,1.0,1.0]);
+        }
+    }
+
+    generateIndicesFromMap() {
+        for(let i = 0; i < this.rawVertexIndices.length; i++) {
+            const indicesString = `${this.rawVertexIndices[i]},${this.rawNormalIndices[i]}`;
+            if (!this.vertexMap.has(indicesString)) {
+                this.vertexMap.set(indicesString,this.vertexMap.size);
+            }
+            //this.vertices.push(...this.rawVertices.slice(this.rawVertexIndices[i]*3, this.rawVertexIndices[i]*3 +3));
+            //this.normals.push(...this.rawNormals.slice(this.rawNormalIndices[i]*3,this.rawNormalIndices[i]*3+3));
+            //this.indices.push(i);
+            //this.colors.push(...[0.0,1.0,1.0,1.0]);
+            //this.colors.push(...[0.0,1.0,1.0,1.0]);
+        }
+    }
+
+    /**
+     * 
+     * @param {String} string 
+     * @param {boolean} normalize 
+     * 
+     * @returns {Shape}
+     */
     parseObjectFromString(string, normalize = false) {
-        
+        //clear all values
         this.reset();
 
-        const fileData = string.split("\n");
+        //split string into lines
+        const data = string.split('\n');
 
-        let objectCounter = 0;
+        for (let i = 0; i < data.length; i++) {
+            const line = data[i].split(' ');
 
-        for(let i = 0; i < fileData.length; i++) {
-            if(fileData[i][0] == 'o') {
-                if(objectCounter < 1) {
-                    objectCounter += 1;
-                    continue;
-                } else {
-                    throw Error("Too many objects in file");
-                }
-            } else if( fileData[i][0] == 'v') {
-                if(fileData[i][1] == ' ') {
-                    this.parseVertex(fileData[i]);
-                } else if (fileData[i][1] == 'n') {
-                    this.parseNormal(fileData[i]);
-                }
-                
-            } else if(fileData[i][0] == 'f') {
-                this.parseFace(fileData[i]);
+            if (line[0] == 'v') {
+                this.parseVertex(line);
+
+            } else if (line[0] == 'vn') {
+                this.parseNormal(line);
+
+            } else if (line[0] == 'f') {
+                this.parseFace(line);
             }
         }
-        
-        this.processVertices(normalize);
 
+        this.normalize();
+
+        this.combine();
+        this.generateIndicesFromMap();
+        console.log(this.rawVertices,this.rawVertexIndices,this.rawNormals,this.rawNormalIndices);
+        console.log("After modification",this.vertices.length,this.normals.length,this.colors.length,this.indices.length,this.vertexMap.size)
         return new Shape(this.vertices,this.normals,this.colors,this.indices);
-    }
-
-    parseVertex(line) {
-        line = line.split(' ');
-
-        const vertex = [
-            parseFloat(line[1]),
-            parseFloat(line[2]),
-            parseFloat(line[3])];
         
-        for(let j = 0; j < vertex.length; j++) {
-            if(vertex[j] < this.min[j]) {
-                this.min[j] = vertex[j];
-            } else if (vertex[j] > this.max[j]) {
-                this.max[j] = vertex[j];
-            }
-        }
-
-        this.vertices.push(...vertex);
-        this.colors.push(...[0.0,1.0,1.0,1.0]);
-    }
-
-    parseNormal(line) {
-        line = line.split(' ');
-
-        const normal = [
-            parseFloat(line[1]),
-            parseFloat(line[2]),
-            parseFloat(line[3])];
-
-        this.normals.push(...normal);
-    }
-
-    parseFace(line) {
-        line = line.split(' ');
-                
-        for(let j = 1; j < line.length; j++) {
-            this.indices.push((parseInt(line[j].split('/')[0])-1));
-        }
-    }
-
-    processVertices(normalize) {
-        const scalingFactor = Math.max(...this.max.map((value, index) => { return 1.0 / (value - this.min[index])}));
-
-        for(let i = 0; i < this.vertices.length; i++) {
-            if(normalize) {
-                this.vertices[i] -= (this.min[i%3] + ((this.max[i%3]-this.min[i%3])/2));
-            }
-            this.vertices[i] *= scalingFactor;
-        }
     }
 }
